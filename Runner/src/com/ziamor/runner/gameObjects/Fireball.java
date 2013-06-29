455298b1802c0d4a417ff6e578ce757aa7a59c96
Rob <Rob@192.168.1.112>
2013-06-29 18:45:58 -0400

    Added arcing fireballs and missiles.

--- Runner/src/com/ziamor/runner/gameObjects/Fireball.java
+++ Runner/src/com/ziamor/runner/gameObjects/Fireball.java
@@ -3,26 +3,39 @@ package com.ziamor.runner.gameObjects;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import com.ziamor.runner.CollisionHandler;
 import com.ziamor.runner.GameObject;
 import com.ziamor.runner.screens.GamePlayScreen;
 
 public class Fireball extends GameObject {
 
 	private int xspeed;
+	private double yspeed;
 
-	public Fireball(int x, int y, int xspeed) {
+	public Fireball(int x, int y, int xspeed, double yspeed) {
 		this.objID = "hazard";
-		width = 32;
+		width = 16;
 		height = 16;
 
 		this.x = x;
 		this.y = y;
 		this.xspeed = xspeed;
+		this.yspeed = yspeed;
 	}
 
 	public void update() {
+		yspeed += 0.35;
 		x += xspeed;
-
+		y += yspeed;
+		
+		// check for collision with wall
+		for (GameObject gameObject : this.parent.getGameObjectsByID("wall")) {
+			if (CollisionHandler.isColliding(this, gameObject)) {
+				// remove this object
+				isActive = false;
+				isVisible = false;
+			}
+		}
 	}
 
 	public void paintComponent(Graphics g) {
